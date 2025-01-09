29759369abae80f8d624aa84e10bd8dee17d7e81
hthomander <tho17074@byui.edu>
2025-01-09 09:38:21 -0700

    gg

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,10 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent = 'last Modified: ${document.lastModified}';
-
 const currentYearSpan = document.getElementById("currentyear");
-const currentYear = new Date().getFullYear();
-currentYearSpan.textContent = currentYear;
-
+if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear();
+}
 
 const lastModifiedParagraph = document.getElementById("lastModified");
-const lastModifiedDate = document.lastModified;
-lastModifiedParagraph.textContent = 'Last Modified: ${lastModifiedDate}';
+if (lastModifiedParagraph) {
+    const lastModifiedDate = document.lastModified;
+    lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
+}
