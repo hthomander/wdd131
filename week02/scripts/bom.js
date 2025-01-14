05d709b2da157fc3337a29cb20bdac082b8f35b7
hthomander <tho17074@byui.edu>
2025-01-14 09:48:00 -0700

    best

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,9 +3,15 @@ const button = document.querySelector('button');
 const list = document.querySelector('#chapters-list');
 
 button.addEventListener('click', function() {
-    const li = document.createElement('li');
 
-    li.textContent = input.ariaValueMax;
+        if (input.value.trim() === '') {
+            alert('Please enter a chapter title');
+            input.focus();
+            return;
+        }
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '❌'
@@ -15,34 +21,9 @@ button.addEventListener('click', function() {
     list.appendChild(li);
 
     input.value = '';
+    input.focus();
 
     deleteButton.addEventListener('click', function() {
         list.removeChild(li);
     });
-
-});
-
-button.addEventListener('click', function(){
-    if (input.value.trim() === '') {
-        alert('Please enter a chapter title');
-        input.focus();
-        return;
-    }
-
-    const li = document.createElement('li');
-    li.textContent = input.value;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = '❌';
-
-    li.appendChild(deleteButton);
-    list.appendChild(li);
-
-    input.value = '';
-    input.focus();
-
-    deleteButton.addEventListener('click', function(){
-        list.removeChild(li);
-    });
-
 });
